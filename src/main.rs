60d2b6de4a155b0d8ce479adbeec73b62264a3f9
Thomas Schmitt <t.e.schmitt@posteo.de>
2025-09-21 16:19:35 +0200

    chore: refactor RLE into module

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,9 @@ use std::path::PathBuf;
 use anyhow::Result;
 use clap::Parser;
 
-#[derive(Debug)]
-struct RleRun {
-    value: u8,
-    count: u64,
-}
+use rle::RleSequence;
 
-#[derive(Debug)]
-struct RleSequence(Vec<RleRun>);
-
-impl From<&[u8]> for RleSequence {
-    fn from(data: &[u8]) -> Self {
-        let mut sequence = Vec::new();
-        if !data.is_empty() {
-            let mut current_value = data[0];
-            let mut current_count = 1;
-
-            for &byte in &data[1..] {
-                if byte == current_value {
-                    current_count += 1;
-                } else {
-                    sequence.push(RleRun {
-                        value: current_value,
-                        count: current_count,
-                    });
-                    current_value = byte;
-                    current_count = 1;
-                }
-            }
-
-            sequence.push(RleRun {
-                value: current_value,
-                count: current_count,
-            });
-        }
-
-        RleSequence(sequence)
-    }
-}
-
-impl Into<Vec<u8>> for RleSequence {
-    fn into(self) -> Vec<u8> {
-        let mut data = Vec::new();
-        for run in self.0 {
-            data.extend(std::iter::repeat(run.value).take(run.count as usize));
-        }
-        data
-    }
-}
+mod rle;
 
 #[derive(Parser, Debug)]
 #[command(
---
+++ src/rle/mod.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+pub(crate) struct RleRun {
+    pub(crate) value: u8,
+    pub(crate) count: u64,
+}
+
+#[derive(Debug)]
+pub(crate) struct RleSequence(Vec<RleRun>);
+
+impl From<&[u8]> for RleSequence {
+    fn from(data: &[u8]) -> Self {
+        let mut sequence = Vec::new();
+        if !data.is_empty() {
+            let mut current_value = data[0];
+            let mut current_count = 1;
+
+            for &byte in &data[1..] {
+                if byte == current_value {
+                    current_count += 1;
+                } else {
+                    sequence.push(RleRun {
+                        value: current_value,
+                        count: current_count,
+                    });
+                    current_value = byte;
+                    current_count = 1;
+                }
+            }
+
+            sequence.push(RleRun {
+                value: current_value,
+                count: current_count,
+            });
+        }
+
+        RleSequence(sequence)
+    }
+}
+
+impl Into<Vec<u8>> for RleSequence {
+    fn into(self) -> Vec<u8> {
+        let mut data = Vec::new();
+        for run in self.0 {
+            data.extend(std::iter::repeat(run.value).take(run.count as usize));
+        }
+        data
+    }
+}
